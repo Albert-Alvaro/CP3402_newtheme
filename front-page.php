783726a28d464a98fbed52943af9b73dcc3201b9
albert-alvaro <albert.alvaro@my.jcu.edu.au>
2023-12-29 12:42:31 +0800

    update and add file

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+
+    Test
+
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                Left Side
+            </div>
+
+            <div class="column">
+                Right Side
+            </div>
+        </div>
+    </div>
+
+<?php get_footer();?>
